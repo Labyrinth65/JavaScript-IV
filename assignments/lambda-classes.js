ca0d2777554de491ee53ca45b469b31947b89d25
Ang Xu <angxu1234@gmail.com>
2019-05-09 13:29:58 -0500

    assignment complete

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,206 @@
 // CODE here for your Lambda Classes
+
+class Person {
+  constructor(a) {
+    this.name = a.name;
+    this.age = a.age;
+    this.location = a.location;
+    this.gender = a.gender;
+  }
+
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
+}
+
+class Instructor extends Person {
+  constructor(a) {
+    super(a);
+    this.specialty = a.specialty;
+    this.favLanguage = a.favLanguage;
+    this.catchPhrase = a.catchPhrase;
+  }
+
+  demo(subject) {
+    return `Today we are learning about ${subject}`;
+  }
+
+  grade(student, subject) {
+    return `${student.name} receives a perfect score on ${subject}`;
+  }
+
+  graded(student) {
+    let change =
+      Math.floor(Math.random() * 10) *
+      Math.floor(Math.random() * 2 >= 1 ? 1 : -1);
+    student.grade = student.grade + change;
+    return `${student.name}'s grade changed by ${change} and is now ${
+      student.grade
+    }`;
+  }
+}
+
+class Student extends Person {
+  constructor(a) {
+    super(a);
+    this.previousBackground = a.previousBackground;
+    this.className = a.className;
+    this.favSubjects = a.favSubjects;
+    this.grade = a.grade;
+  }
+
+  listsSubjects() {
+    return this.favSubjects.join("");
+  }
+
+  PRAssignment(subject) {
+    return `${this.name} has submitted a PR for ${subject}`;
+  }
+
+  sprintChallenge(subject) {
+    return `${this.name} has begun sprint challenge on ${subject}`;
+  }
+
+  graduate() {
+    if (this.grade >= 70) return `${this.name} has graduated!`;
+    else return `Keep grading assignments!`;
+  }
+}
+
+class ProjectManagers extends Instructor {
+  constructor(a) {
+    super(a);
+    this.gradClassName = a.gradClassName;
+    this.favInstructor = a.favInstructor;
+  }
+
+  standUp(slack) {
+    return `${this.name} announces to ${slack}, @channel standy times!`;
+  }
+
+  debugsCode(student, subject) {
+    return `${this.name} debugs ${student.name}'s code on ${subject}`;
+  }
+}
+
+const Adam = new Instructor({
+  name: "Adam",
+  location: "Austin",
+  age: 30,
+  gender: "male",
+  favLanguage: "JavaScript",
+  specialty: "Front-end",
+  catchPhrase: `Don't forget the homies`
+});
+
+const Brian = new Instructor({
+  name: "Brian",
+  location: "Boston",
+  age: 31,
+  gender: "male",
+  favLanguage: "JavaScript II",
+  specialty: "Back-end",
+  catchPhrase: `Don't forget the roomies`
+});
+
+const Carol = new Instructor({
+  name: "Carol",
+  location: "Chicago",
+  age: 32,
+  gender: "female",
+  favLanguage: "JavaScript III",
+  specialty: "Middle-end",
+  catchPhrase: `Don't forget the boonies`
+});
+
+const Ditto = new Student({
+  name: "Ditto",
+  location: "Denver",
+  age: 33,
+  gender: "???",
+  favSubjects: [
+    "JavaScript",
+    "Transformations",
+    "How To Tackle",
+    "How To Hyper Beam"
+  ],
+  className: "web18",
+  previousBackground: "pokemon",
+  grade: 50
+});
+
+const Frank = new Student({
+  name: "Frank",
+  location: "Fort Worth",
+  age: 34,
+  gender: "male",
+  favSubjects: ["JavaScript II", "Vigilante Training"],
+  className: "web19",
+  previousBackground: "train vigilantes",
+  grade: 80
+});
+
+const Grace = new Student({
+  name: "Grace",
+  location: "Fort Worth",
+  age: 35,
+  gender: "female",
+  favSubjects: ["JavaScript III", "Catching Bad Guys"],
+  className: "web20",
+  previousBackground: "catch bad guys",
+  grade: 90
+});
+
+const Hope = new ProjectManagers({
+  name: "Hope",
+  location: "Havana",
+  age: 36,
+  gender: "female",
+  favLanguage: "JavaScript IV",
+  specialty: "Upper-end",
+  catchPhrase: `Don't forget the goonies`,
+  gradClassName: "web14",
+  favInstructor: Adam
+});
+
+const Isaac = new ProjectManagers({
+  name: "Isaac",
+  location: "Indianapolis",
+  age: 37,
+  gender: "male",
+  favLanguage: "JavaScript V",
+  specialty: "Lower-end",
+  catchPhrase: `Don't forget the cooties`,
+  gradClassName: "web15",
+  favInstructor: Brian
+});
+
+const Jane = new ProjectManagers({
+  name: "Jane",
+  location: "Jupiter",
+  age: 999,
+  gender: "ULTIMATE GENDER",
+  favLanguage: "ULTIMATE JAVASCRIPT",
+  specialty: "ULTIMATE END",
+  catchPhrase: `Yo`,
+  gradClassName: "webULTIMATE",
+  favInstructor: Carol
+});
+
+console.log(Adam.grade(Ditto, Ditto.favSubjects[0]));
+console.log(Brian.demo(Brian.specialty));
+console.log(Carol);
+console.log(Ditto);
+console.log(Ditto.listsSubjects);
+console.log(Frank.PRAssignment("Vigilantes Are People Too"));
+console.log(
+  Grace.sprintChallenge("Catching Bad Guys Won't Fill The Empty Hole Inside")
+);
+console.log(Hope.standUp("web20"));
+console.log(Isaac.debugsCode(Ditto, Ditto.favSubjects[1]));
+console.log(Jane);
+console.log(Carol.graded(Ditto));
+console.log(Jane.graded(Ditto));
+console.log(Ditto.grade);
+console.log(Ditto.graduate());
+console.log(Grace.graduate());
--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -7,3 +7,159 @@ Prototype Refactor
 2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
 
 */
+class GameObject {
+  constructor(a) {
+    this.createdAt = a.createdAt;
+    this.name = a.name;
+    this.dimensions = a.dimensions;
+  }
+
+  destroy() {
+    return `${this.name} was removed from the game.`;
+  }
+}
+
+class CharacterStats extends GameObject {
+  constructor(a) {
+    super(a);
+    this.healthPoints = a.healthPoints;
+  }
+
+  takeDamage() {
+    return `${this.name} took damage.`;
+  }
+}
+
+class Humanoid extends CharacterStats {
+  constructor(a) {
+    super(a);
+    this.team = a.team;
+    this.weapons = a.weapons;
+    this.language = a.language;
+  }
+
+  greet() {
+    return `${this.name} offers a greeting in ${this.language}.`;
+  }
+}
+
+const mage = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 1,
+    height: 1
+  },
+  healthPoints: 5,
+  name: "Bruce",
+  team: "Mage Guild",
+  weapons: ["Staff of Shamalama"],
+  language: "Common Tongue"
+});
+
+const swordsman = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 2,
+    height: 2
+  },
+  healthPoints: 15,
+  name: "Sir Mustachio",
+  team: "The Round Table",
+  weapons: ["Giant Sword", "Shield"],
+  language: "Common Tongue"
+});
+
+const archer = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 1,
+    width: 2,
+    height: 4
+  },
+  healthPoints: 10,
+  name: "Lilith",
+  team: "Forest Kingdom",
+  weapons: ["Bow", "Dagger"],
+  language: "Elvish"
+});
+
+console.log(mage);
+
+console.log(mage.createdAt); // Today's date
+console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+console.log(swordsman.healthPoints); // 15
+console.log(mage.name); // Bruce
+console.log(swordsman.team); // The Round Table
+console.log(mage.weapons); // Staff of Shamalama
+console.log(archer.language); // Elvish
+console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+console.log(mage.takeDamage()); // Bruce took damage.
+console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+
+class Hero extends Humanoid {
+  constructor(a) {
+    super(a);
+  }
+
+  holySpell(target) {
+    target.healthPoints = target.healthPoints - 20;
+    if (target.healthPoints <= 0) {
+      return target.destroy(target);
+    } else
+      return `${target.takeDamage(target)} ${
+        target.healthPoints
+      } hp remaining.`;
+  }
+}
+
+class Villain extends Humanoid {
+  constructor(a) {
+    super(a);
+  }
+
+  fireBall(target) {
+    target.healthPoints = target.healthPoints - 10;
+    if (target.healthPoints <= 0) {
+      return target.destroy(target);
+    } else
+      return `${target.takeDamage(target)} ${
+        target.healthPoints
+      } hp remaining.`;
+  }
+}
+
+const paladin = new Hero({
+  createdAt: new Date(),
+  dimensions: {
+    length: 5,
+    width: 5,
+    height: 5
+  },
+  healthPoints: 50,
+  name: "Pally",
+  team: "Team 1",
+  weapons: ["Sword", "Shield"],
+  language: "Hero"
+});
+
+const wizard = new Villain({
+  createdAt: new Date(),
+  dimensions: {
+    length: 5,
+    width: 5,
+    height: 5
+  },
+  healthPoints: 40,
+  name: "Vivi",
+  team: "Team 2",
+  weapons: ["Staff", "Wand"],
+  language: "Wizard"
+});
+
+console.log(paladin.holySpell(wizard));
+console.log(wizard.healthPoints);
+console.log(wizard.fireBall(paladin));
+console.log(paladin.healthPoints);
+console.log(paladin.holySpell(wizard));
